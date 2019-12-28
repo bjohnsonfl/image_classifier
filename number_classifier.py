250fc0dbfefefb14b9867aaba111712e4c9d9360
BLake J <bjohnson293@ufl.edu>
2019-12-28 11:02:03 -0500

    structure of layers class created

--- number_classifier.py
+++ number_classifier.py
@@ -5,12 +5,65 @@ from __future__ import print_function
 import numpy as np
 
 
+def logReg():
+    in_size = 5  # length * width of image, but 1 for now
+    num_Of_Input = 3  # number of images
+    out_size = 2  # size of output
+    step_Size = 0.1  # learning rate
+    iter = 1  # iterations of gradient decent
+
+    X = np.random.randint(0, 2, (in_size, num_Of_Input))  # input layer, tested with random int 0 to 1 for grayscale
+    W = np.random.randn(in_size, out_size)  # number of weights as input
+    B = np.random.randn(out_size, 1)
+    Y = np.random.rand(out_size, num_Of_Input)  # output_layer_truths where rows are outputs and col are examples
+
+    for _ in range(0, iter):
+        # vectorized feedforward. W transpose weights * X training examples + B
+        Z = np.dot(W.T, X) + B
+        A = 1 / (1 + np.exp(-Z))
+
+        # Loss/Cost Function
+        # i x j  matrix where i is number of outputs and j are examples
+        J = -(Y * np.log(A) + ((1 - Y) * np.log(1 - A)))
+        J = np.sum(J, axis=1, keepdims=True) / num_Of_Input
+        # J = J.T # Make the output vertical
+
+        # Gradient Decent Single Iteration
+        dZ = A - Y # only for output layer. dz1 = (W2.T * dz2) .* g1'(z1) where g(z) is activation func
+        dW = (1 / num_Of_Input) * np.dot(X, dZ.T)  # each column are for each additional neuron in the layer
+        dB = (1 / num_Of_Input) * np.sum(dZ, axis=1, keepdims=True)
+
+        W = W - step_Size * dW
+        B = B - step_Size * dB
+
+
+class layer:
+    """
+    A layer initial parameters will dictate the size of the layer,
+    and in the future, the type of activation function.
+
+    The layer block diagram will consist of the following:
+        input: A[n-1]  (internal signal will be X)
+        output: A[n]
+        feedback: dZ[n], W[n]  (this is used for gradient decent in layer [n-1])
+
+        internal variables
+            W, a (ixj) matrix where i is the size of layer n-1 and j is the size of the layer n
+            B, a (ix1) matrix where i is the size of the layer n
+            dZ, a (ixj) matrix where i is the size of the layer n and j are the number of examples
+            dW, a (ixj) matrix where i is the size of layer n-1 and j is the size of layer n
+            dB, a (ixj) matrix where i is the size of the layer n
+
+            dZ dW and dB are the gradients of Z W and B which are used to with the learning rate to step to a min.
+    """
+
+
 def main():
     in_size = 5  # length * width of image, but 1 for now
-    num_Of_Input = 2 # number of images
+    num_Of_Input = 3 # number of images
     out_size = 2  # size of output
     step_Size = 0.1 # learning rate
-    iter = 100 # iterations of gradient decent
+    iter = 1 # iterations of gradient decent
 
 
     X = np.random.randint(0, 2, (in_size, num_Of_Input))  # input layer, tested with random int 0 to 1 for grayscale
@@ -29,7 +82,7 @@ def main():
         Z = np.dot(W.T, X) + B
 
         A = 1 / (1 + np.exp(-Z))
-        #print("A: \n", A)
+        print("A: \n", A)
 
         # Loss/Cost Function
         #i x j  matrix where i is number of outputs and j are examples
@@ -40,10 +93,12 @@ def main():
         print("J: \n", J, "\n")
 
         # Gradient Decent Single Iteration
-        dZ = A - Y
-        #print("dZ: \n", dZ, "\n")
+        dZ = A - Y # only for output layer. dz1 = (W2.T * dz2) .* g1'(z1) where g(z) is activation func
+        print("dZ: \n", dZ, "\n")
+        print("X: \n", X, "\n")
+        print("XT: \n", X.T, "\n")
         dW = (1/num_Of_Input) * np.dot(X, dZ.T) # each column are for each additional neuron in the layer
-        #print("dW: \n", dW, "\n")
+        print("dW: \n", dW, "\n")
         dB = (1/num_Of_Input) * np.sum(dZ, axis=1, keepdims=True)
         #print("dB: \n", dB, "\n")
 
